a0d7060c16222c5c2471766d5d9a1fd7c57fe588
juanrarodriguez18 <dragondr3w@gmail.com>
2016-12-09 08:50:53 +0100

    Añadido proyecto Maven

---
+++ practicaEGC/src/main/java/es/egc/app/App.java
@@ -0,0 +1,13 @@
+package es.egc.app;
+
+public class App 
+{
+
+    static int getVal(){
+        return 1;
+    }
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
