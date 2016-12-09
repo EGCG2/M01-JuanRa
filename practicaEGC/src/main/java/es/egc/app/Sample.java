f0421ad6f5cb2453869ff23986f251e763bb634b
juanrarodriguez18 <dragondr3w@gmail.com>
2016-12-09 09:50:27 +0100

    Añadido plugin y añadida su automatización

---
+++ practicaEGC/src/main/java/es/egc/app/Sample.java
@@ -0,0 +1,12 @@
+package es.egc.app;
+
+/**
+ * Unit test for simple App.
+ */
+public class Sample{
+    public void testApp()
+    {
+        String[] arguments = new String[] {"123"};
+        App.main(arguments);
+    }
+}
