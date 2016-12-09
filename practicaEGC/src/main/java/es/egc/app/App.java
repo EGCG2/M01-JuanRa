8ad4e73046a1d88fee1142702305a050e97504e0
juanrarodriguez18 <dragondr3w@gmail.com>
2016-12-09 09:05:34 +0100

    Añadidas dos dependencias al proyecto Maven

--- practicaEGC/src/main/java/es/egc/app/App.java
+++ practicaEGC/src/main/java/es/egc/app/App.java
@@ -1,13 +1,15 @@
 package es.egc.app;
+import org.apache.log4j.*;
 
 public class App 
 {
-
+    static Logger log=Logger.getLogger(App.class); 
     static int getVal(){
         return 1;
     }
     public static void main( String[] args )
     {
+        log.info("Returning 1");
         System.out.println( "Hello World!" );
     }
 }
