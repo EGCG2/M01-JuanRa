aa48ba5417c5f1aeb18eaf8af89fcc729adf3e52
juanrarodriguez18 <dragondr3w@gmail.com>
2016-12-09 09:15:56 +0100

    Añadido código de jGraphT

--- practicaEGC/src/main/java/es/egc/app/App.java
+++ practicaEGC/src/main/java/es/egc/app/App.java
@@ -1,5 +1,8 @@
 package es.egc.app;
+import java.net.*;
 import org.apache.log4j.*;
+import org.jgrapht.*;
+import org.jgrapht.graph.*;
 
 public class App 
 {
@@ -11,5 +14,66 @@ public class App
     {
         log.info("Returning 1");
         System.out.println( "Hello World!" );
+
+        UndirectedGraph<String, DefaultEdge> stringGraph = createStringGraph();
+
+        // note undirected edges are printed as: {<v1>,<v2>}
+        System.out.println(stringGraph.toString());
+
+        // create a graph based on URL objects
+        DirectedGraph<URL, DefaultEdge> hrefGraph = createHrefGraph();
+
+        // note directed edges are printed as: (<v1>,<v2>)
+        System.out.println(hrefGraph.toString());
+    }
+
+     private static DirectedGraph<URL, DefaultEdge> createHrefGraph()
+    {
+        DirectedGraph<URL, DefaultEdge> g =
+            new DefaultDirectedGraph<URL, DefaultEdge>(DefaultEdge.class);
+
+        try {
+            URL amazon = new URL("[http://www.amazon.com](http://www.amazon.com)");
+            URL yahoo = new URL("[http://www.yahoo.com](http://www.yahoo.com)");
+            URL ebay = new URL("[http://www.ebay.com](http://www.ebay.com)");
+
+            // add the vertices
+            g.addVertex(amazon);
+            g.addVertex(yahoo);
+            g.addVertex(ebay);
+
+            // add edges to create linking structure
+            g.addEdge(yahoo, amazon);
+            g.addEdge(yahoo, ebay);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return g;
+    }
+
+    private static UndirectedGraph<String, DefaultEdge> createStringGraph()
+    {
+        UndirectedGraph<String, DefaultEdge> g =
+            new SimpleGraph<String, DefaultEdge>(DefaultEdge.class);
+
+        String v1 = "v1";
+        String v2 = "v2";
+        String v3 = "v3";
+        String v4 = "v4";
+
+        // add the vertices
+        g.addVertex(v1);
+        g.addVertex(v2);
+        g.addVertex(v3);
+        g.addVertex(v4);
+
+        // add edges to create a circuit
+        g.addEdge(v1, v2);
+        g.addEdge(v2, v3);
+        g.addEdge(v3, v4);
+        g.addEdge(v4, v1);
+
+        return g;
     }
 }
